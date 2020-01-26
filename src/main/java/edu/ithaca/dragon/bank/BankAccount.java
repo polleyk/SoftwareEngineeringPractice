5b6b740f0dc73fec1c113879b5b23b5f1eda23a3
Vera Crabtree <vcrabtree99@gmail.com>
2020-01-26 13:01:06 -0500

    Added insufficient funds

--- src/main/java/edu/ithaca/dragon/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dragon/bank/BankAccount.java
@@ -29,39 +29,74 @@ public class BankAccount {
     /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
-    public void withdraw (double amount)  {
-        balance -= amount;
-
+    public void withdraw (double amount) throws InsufficientFundsException {
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            throw new InsufficientFundsException("Not enough money");
+        }
     }
 
 
     public static boolean isEmailValid(String email){
         int length = email.length();
+        int currIndex = email.indexOf('@');
 
-        if (email.indexOf('@') == -1){
+        if (currIndex == -1) {
             return false;
         }
-        else if (email.contains("-@")) {
+        else if (currIndex == 0) {
             return false;
         }
-        else if (email.indexOf('@') == length - 1) {
+        else if (currIndex == length - 1) {
             return false;
-        }
-        else if (email.indexOf('@') == 0) {
-            return false;
-        }
-        else if (email.contains("..")) {
-            return false;
-        }
-        else if (email.contains("#")) {
-            return false;
-        }
-        else if (email.indexOf('.') == 0) {
-            return false;
-        }
-        else {
-            return true;
         }
 
+        int dot = email.lastIndexOf('.');
+        if (currIndex > dot) {
+            return false;
+        }
+        else if (currIndex > email.length() - 3) {
+            return false;
+        }
+
+        String[] emailSplit = email.split("@");
+        for (int currEmail = 0; currEmail < emailSplit.length; currEmail++) {
+
+            String currPiece = emailSplit[currEmail];
+            for (int charIndex = 0; charIndex < currPiece.length(); charIndex++) {
+                char currChar = currPiece.charAt(charIndex);
+
+                Boolean isValid = false;
+
+                if (currChar == 45 || currChar == 46 || currChar == 95) {
+                    isValid = true;
+                    if (charIndex == 0 || charIndex == currPiece.length() - 1) {
+                        isValid = false;
+                    } else {
+                        char left = currPiece.charAt(charIndex - 1);
+                        char right = currPiece.charAt(charIndex + 1);
+                        if (left == 45 || left == 46 || left == 95 || right == 45 || right == 46 || right == 95) {
+                            isValid = false;
+                        }
+                    }
+
+                }
+                else if (currChar >= 48 && currChar <= 57) {
+                    isValid = true;
+                }
+                else if (currChar >= 65 && currChar <= 90) {
+                    isValid = true;
+                }
+                else if (currChar >= 97 && currChar <= 122) {
+                    isValid = true;
+                }
+
+                if (!isValid) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
